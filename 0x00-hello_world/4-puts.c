64a18dc3e5f1c8b41a5220567863bb0acbd4fb68
rayantaha16 <rayantaha16@gmail.com>
2023-08-10 11:01:26 -0700

    hhhh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - entry point
- * Description: this is puts fun
- * return: always return 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
