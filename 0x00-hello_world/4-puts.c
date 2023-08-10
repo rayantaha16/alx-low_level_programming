24f9e432a1cdd90d4b1a5059a5c37b9426318a09
rayantaha16 <rayantaha16@gmail.com>
2023-08-10 10:40:04 -0700

    gg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - entry point
  * Description: this is puts fun
+ * return: always return 0 (success)
  */
-int main (void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
