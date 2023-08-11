15fab91709e151fecf8ce58baba88d468f89812e
rayantaha16 <rayantaha16@gmail.com>
2023-08-11 10:22:29 -0700

    a

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+		putchar(c);
+	putchar(10);
+	return (0);
+}
