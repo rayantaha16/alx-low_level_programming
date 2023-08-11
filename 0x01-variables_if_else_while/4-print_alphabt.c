637d6292fb7b2446a74daa8aaf8961a9d5955ec0
rayantaha16 <rayantaha16@gmail.com>
2023-08-11 09:02:11 -0700

    four

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 97; c < 123; c++)
+	{
+		if (c != 101 || c != 113)
+			putchar(c);
+	}
+}
