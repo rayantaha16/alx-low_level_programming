f7d37786d4c4d23e0ab94c100092dc0d86f036fa
rayantaha16 <rayantaha16@gmail.com>
2023-08-25 06:58:43 -0700

    ;;

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void){
+int main(void)
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -14,5 +15,5 @@ int main(void){
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return 0;
+	return (0);
 }
