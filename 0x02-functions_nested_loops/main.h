f6e86d2a0a2062baeab4291473c666838703cf5c
rayantaha16 <rayantaha16@gmail.com>
2023-08-25 03:23:04 -0700

    a

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,13 @@
-#define MAIN_H
-#include <stdio.h>
-int pr(){
-	printf("_putchat\n");
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
