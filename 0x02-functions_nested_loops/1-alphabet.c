e234c2a8b3dfff97e2e505c1f5a2d12777c908c2
rayantaha16 <rayantaha16@gmail.com>
2023-08-25 07:10:18 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Program prints lowercase alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	int a, n;
+
+	for (a = 97; a < 122; a++)
+	{
+		_putchar(a);
+		_putchar(n);
+	}
+	return (0);
+}
