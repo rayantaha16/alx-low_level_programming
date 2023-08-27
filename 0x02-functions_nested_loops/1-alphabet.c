27b86f24fec40ba0e6fac7a82869f5cead07afac
rayantaha16 <rayantaha16@gmail.com>
2023-08-27 08:30:08 -0700

    l

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,13 @@
  *
  * Return: Always 0 (Success)
  */
-	void print_alphabet(void)
-	{
-		int d;
+void print_alphabet(void)
+{
+	int d;
 
-		for (d = 97; d < 123; d++)
-		{
-			_putchar(d);
-		}
-		_putchar(10);
+	for (d = 97; d < 123; d++)
+	{
+		_putchar(d);
 	}
+	_putchar(10);
+}
