685520c176b90ccbdae1ee925ecf89d2f8a86e9a
rayantaha16 <rayantaha16@gmail.com>
2023-08-29 06:27:38 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int d;
+
+	int l;
+
+	for (l = 1; l < 11; l++)
+	{
+		for (d = 97; d < 123; d++)
+		{
+			_putchar(d);
+		}
+	}
